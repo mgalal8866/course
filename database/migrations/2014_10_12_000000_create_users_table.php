92b1a22f11b6aaf2a4fbb59db4bdb53cb7d571dd
mgalal8866 <mgalal8866@gmail.com>
2023-12-07 06:27:15 +0200

    new file:   app/Models/Blog.php 	new file:   app/Models/Category.php 	new file:   app/Models/CourseTrainers.php 	new file:   app/Models/CourseUsers.php 	new file:   app/Models/Courses.php 	new file:   app/Models/Faq.php 	new file:   app/Models/Lessons.php 	new file:   app/Models/PaymentMethods.php 	new file:   app/Models/Payments.php 	new file:   app/Models/Review.php 	new file:   app/Models/Setting.php 	new file:   app/Models/Trainer.php 	modified:   database/migrations/2014_10_12_000000_create_users_table.php 	new file:   database/migrations/2023_12_07_032009_create_categories_table.php 	new file:   database/migrations/2023_12_07_032847_create_courses_table.php 	new file:   database/migrations/2023_12_07_032902_create_lessons_table.php 	new file:   database/migrations/2023_12_07_033053_create_trainers_table.php 	new file:   database/migrations/2023_12_07_033119_create_settings_table.php 	new file:   database/migrations/2023_12_07_033157_create_faqs_table.php 	new file:   database/migrations/2023_12_07_033217_create_blogs_table.php 	new file:   database/migrations/2023_12_07_033242_create_reviews_table.php 	new file:   database/migrations/2023_12_07_034243_create_payments_table.php 	new file:   database/migrations/2023_12_07_041637_create_course_trainers_table.php 	new file:   database/migrations/2023_12_07_041751_create_course_users_table.php 	new file:   database/migrations/2023_12_07_042156_create_payment_methods_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->unique();
             $table->string('email')->unique();
+            $table->string('phone_parent')->unique();
+            $table->string('email_parent')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
