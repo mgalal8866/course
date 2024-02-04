319a04fb81ddcd8ef606453c5a6c67c9da8e4e16
mgalal8866 <mgalal8866@gmail.com>
2024-02-04 09:27:36 +0200

    modified:   app/Repository/DBFreeCourseRepository.php

--- app/Repository/DBFreeCourseRepository.php
+++ app/Repository/DBFreeCourseRepository.php
@@ -5,19 +5,24 @@ namespace App\Repository;
 use App\Models\FreeCourse;
 use Illuminate\Database\Eloquent\Model;
 use App\Repositoryinterface\FreeCourseRepositoryinterface;
+use Illuminate\Http\Request;
 
 class DBFreeCourseRepository implements FreeCourseRepositoryinterface
 {
 
     protected Model $model;
-    public function __construct(FreeCourse $model)
+    protected $request;
+
+
+    public function __construct(FreeCourse $model,Request $request)
     {
         $this->model = $model;
+        $this->request = $request;
     }
 
     public function get_free_course_by_category($id){
-
-        return $this->model->whereCategoryId($id)->paginate(1);
+        $perPage = $this->request->input('per_page', 10);
+        return $this->model->whereCategoryId($id)->paginate($perPage);
     }
     public function get_free_course_by_id($id){
 
