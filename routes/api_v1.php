6be4bdaf370e5a2f29660b7f75150dd2f36614a1
mgalal8866 <mgalal8866@gmail.com>
2024-02-04 08:14:00 +0200

    modified:   routes/api_v1.php

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -34,6 +34,7 @@ Route::get('/slider',[HomeController::class,'getslider'])->name('getslider');
 Route::get('/setting',[HomeController::class,'getsetting'])->name('getsetting');
 Route::get('/category/free/course',[CategoryFreeCourseController::class,'getcategoryfreecourse']);
 Route::get('/free/course/bycategory/{id?}',[FreeCourseController::class,'get_free_course_by_category']);
+Route::get('/free/course/{id?}',[FreeCourseController::class,'get_free_course_by_id']);
 Route::middleware(['jwt.verify'])->group(function () {
 
 
