e17ab227f8532a04659519c974a55209ed134e0e
mgalal8866 <mgalal8866@gmail.com>
2024-02-06 02:57:32 +0200

    modified:   app/Repository/DBCourseRepository.php

--- app/Repository/DBCourseRepository.php
+++ app/Repository/DBCourseRepository.php
@@ -5,20 +5,26 @@ namespace App\Repository;
 use App\Models\Courses;
 use Illuminate\Database\Eloquent\Model;
 use App\Repositoryinterface\CourseRepositoryinterface;
-
+use Illuminate\Http\Request;
 
 class DBCourseRepository implements CourseRepositoryinterface
 {
 
     protected Model $model;
-    public function __construct(Courses $model)
+    protected $request;
+    
+    public function __construct(Courses $model,Request $request)
     {
         $this->model = $model;
+        $this->request = $request;
+
     }
 
     public function getcoursesbycategroy($category_id)
     {
-       return $this->model->where('category_id',$category_id)->get();
+        $perPage = $this->request->input('per_page', 20);
+        return $this->model->whereCategoryId($category_id)->paginate($perPage);
+    //    return $this->model->where('category_id',$category_id)->get();
     }
     public function getcoursebyid($id)
     {
