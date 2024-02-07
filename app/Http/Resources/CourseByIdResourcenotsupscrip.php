b782731e5c16bd159199c5deb738ee0782ff54fb
mgalal8866 <mgalal8866@gmail.com>
2024-02-07 08:40:21 +0200

    modified:   app/Http/Resources/CourseByIdResource.php 	new file:   app/Http/Resources/CourseByIdResourcenotsupscrip.php 	modified:   resources/views/dashboard/test.blade.php

---
+++ app/Http/Resources/CourseByIdResourcenotsupscrip.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CourseByIdResourcenotsupscrip extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'course_id'         => $this->id,
+            'course_name'       => $this->name,
+            'course_image'      => $this->imageurl ?? '',
+            'course_price'      => $this->price ?? '',//كاتب
+            'course_category'   => $this->category->name ?? '',
+            'short_description' => $this->short_description ?? '',
+            'schedule'          => $this->schedule ?? '',
+            'description'       => $this->description ?? '',
+            'features'          => 'features-features-features-features-features' ?? '',
+            'start_date'        => $this->start_date ?? '',
+            'end_date'          => $this->end_date ?? '',
+            'duration'          => $this->duration_course ?? '',
+            'conditions'        => $this->conditions ?? '',
+            'validity'          => $this->validity ?? '',
+            'free_tutorial'     => $this->file_free ?? '',
+            'free_file'         => $this->file_free ?? '',
+            'max_drainees'      => $this->max_drainees ?? '',
+            'created_at'        => $this->created_at->format('d/m/Y'),
+            'course_subscripe' => 'false',
+
+        ];
+    }
+}
--- resources/views/dashboard/test.blade.php
+++ resources/views/dashboard/test.blade.php
@@ -1,18 +1,18 @@
 <div>
    @push('csslive')
-   <link rel="stylesheet" type="text/css" href="{{ asset('asset/summernote/summernote-bs5.css') }}">
+   <link rel="stylesheet" type="text/css" href="{{ asset('asset/summernote/summernote-bs4.css') }}">
 
    @endpush
    <div id="summernote"></div>
   @push('jslive')
- 
+
     <script>
       $('#summernote').summernote({
-        placeholder: 'Hello Bootstrap 5',
+        placeholder: 'Hello Bootstrap 4',
         tabsize: 2,
         height: 100
       });
     </script>
-   <script src="{{ asset('asset/summernote/ummernote-bs5.js')}}"></script>
+   <script src="{{ asset('asset/summernote/ummernote-bs4.js')}}"></script>
    @endpush
 </div>
