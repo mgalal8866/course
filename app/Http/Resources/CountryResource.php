06ef3b8b0257503796c0c486b7a706a315c5f2ec
mgalal8866 <mgalal8866@gmail.com>
2024-02-17 03:24:33 +0200

    modified:   app/Http/Resources/CountryResource.php

--- app/Http/Resources/CountryResource.php
+++ app/Http/Resources/CountryResource.php
@@ -16,6 +16,8 @@ class CountryResource extends JsonResource
             'country_id'        => $this->id ,
             'country_name'      => $this->name ,
             'country_phonecode' => $this->phonecode ,
+            'country_code2'     => $this->iso2 ,
+            'country_code3'     => $this->iso3 ,
             'country_currency'  => $this->currency[Lang::locale()] ,
         ];
     }
