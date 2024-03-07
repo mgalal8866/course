9b9ec7160d3026527ce3911c316520ae6dcbc8e6
mgalal8866 <mgalal8866@gmail.com>
2024-03-07 23:48:46 +0200

    modified:   app/Http/Controllers/Api/V1/UsersController.php

--- app/Http/Controllers/Api/V1/UsersController.php
+++ app/Http/Controllers/Api/V1/UsersController.php
@@ -18,9 +18,9 @@ class UsersController extends Controller
         $this->users = $Users;
     }
 
-    public function login(Request $request)
+    public function login(LoginUserRequest $request)
     {
-        Log::error( $request->all());
+     
       return  $this->users->login($request);
     }
     public function signup(UserRequest $request)
