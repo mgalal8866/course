abd043f1456d0e07a9ddbeeda8ce1c2c761cb2ad
mgalal8866 <mgalal8866@gmail.com>
2024-03-20 03:53:31 +0200

    modified:   app/Http/Resources/Course/CollectionCourseResource.php

--- app/Http/Resources/Course/CollectionCourseResource.php
+++ app/Http/Resources/Course/CollectionCourseResource.php
@@ -15,15 +15,15 @@ class CollectionCourseResource extends JsonResource
         return [
             'course' =>  isset($this['data'][0]['childrens'][0]['courses'][0])? new CourseResource( $this['data'][0]['childrens'][0]['courses'][0]):[],
             'course_files'      => [
-                'file_supplementary' => $this['data'][0]['childrens'][0]['courses'][0]['file_supplementary'] ?? '',
-                'file_aggregates'    => $this['data'][0]['childrens'][0]['courses'][0]['file_aggregates'] ?? '',
-                'file_explanatory'   => $this['data'][0]['childrens'][0]['courses'][0]['file_explanatory'] ?? '',
-                'file_work'          => $this['data'][0]['childrens'][0]['courses'][0]['file_work'] ?? '',
-                'file_test'          => $this['data'][0]['childrens'][0]['courses'][0]['file_test'] ?? '',
-                'file_free'          => $this['data'][0]['childrens'][0]['courses'][0]['file_free'] ?? ''
+                'file_supplementary' => isset( $this['data'][0]['childrens'][0]['courses'][0]['file_supplementary']) ?? '',
+                'file_aggregates'    =>  isset($this['data'][0]['childrens'][0]['courses'][0]['file_aggregates']) ?? '',
+                'file_explanatory'   =>  isset($this['data'][0]['childrens'][0]['courses'][0]['file_explanatory']) ?? '',
+                'file_work'          => isset( $this['data'][0]['childrens'][0]['courses'][0]['file_work']) ?? '',
+                'file_test'          =>  isset($this['data'][0]['childrens'][0]['courses'][0]['file_test']) ?? '',
+                'file_free'          =>  isset($this['data'][0]['childrens'][0]['courses'][0]['file_free']) ?? ''
             ],
             'stages' => ParentStagesResource::collection( $this['data']),
-            'comments' => $this['data'][0]['childrens'][0]['courses'][0]['comments']?CommentsResource::collection( $this['data'][0]['childrens'][0]['courses'][0]['comments']):[],
+            'comments' =>  isset($this['data'][0]['childrens'][0]['courses'][0]['comments'])?CommentsResource::collection( $this['data'][0]['childrens'][0]['courses'][0]['comments']):[],
             'triners' => isset($this['data'][0]['childrens'][0]['courses'][0])?TrainerResource::collection( $this['data'][0]['childrens'][0]['courses'][0]['coursetrainers']):[],
 
         ];
