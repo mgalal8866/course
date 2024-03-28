58d385411ed8a5d68429aab78d402f9799d789c1
mgalal8866 <mgalal8866@gmail.com>
2024-03-28 03:56:49 +0200

    modified:   app/Http/Middleware/Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,13 +10,13 @@ class Cors
 
     public function handle( $request, Closure $next)
     {
-      
+
         $response = $next($request);
-        $response->header('X-Frame-Options', 'SAMEORIGIN');
-        $response->header('Content-Security-Policy', 'frame-ancestors *');
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Headers', 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        // $response->header('X-Frame-Options', 'SAMEORIGIN');
+        // $response->header('Content-Security-Policy', 'frame-ancestors *');
+        // $response->header('Access-Control-Allow-Origin', '*');
+        // $response->header('Access-Control-Allow-Headers', 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
+        // $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         return  $response;
     }
 }
