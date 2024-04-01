0303f44e7d55123b9aecc5dbe9fb434dc3646f06
mgalal8866 <mgalal8866@gmail.com>
2024-04-01 05:08:25 +0200

    modified:   app/Http/Controllers/Api/V1/HomeController.php

--- app/Http/Controllers/Api/V1/HomeController.php
+++ app/Http/Controllers/Api/V1/HomeController.php
@@ -104,58 +104,58 @@ class HomeController extends Controller
         $data   = Slider::where('active',1)->get();
         return Resp( SliderResource::collection($data), 'success');
     }
-    public function section2()
-    {
-        $data = [];
-        $data = getsetting('section2_setting', ['section2_status', 'section2_title', 'section2_image', 'section2_body']);
-        return Resp(new HomeSection2Resource($data), 'success');
-    }
-    public function section3()
-    {
-        $data['setting']  = getsetting('section3_setting', ['section3_status', 'section3_title', 'section3_body', 'section4_image']);
-        $data['category'] = CategoryCourseResource::collection(Category::withCount('courses')->get());;
-        return Resp(new HomeSection3Resource($data), 'success');
-    }
-    public function section4()
-    {
-        $data['setting']    = getsetting('section4_setting', ['section4_status', 'section4_title', 'section4_body', 'section4_image']);
-        return Resp(new HomeSection4Resource($data), 'success');
-    }
-    public function section5()
-    {
+    // public function section2()
+    // {
+    //     $data = [];
+    //     $data = getsetting('section2_setting', ['section2_status', 'section2_title', 'section2_image', 'section2_body']);
+    //     return Resp(new HomeSection2Resource($data), 'success');
+    // }
+    // public function section3()
+    // {
+    //     $data['setting']  = getsetting('section3_setting', ['section3_status', 'section3_title', 'section3_body', 'section4_image']);
+    //     $data['category'] = CategoryCourseResource::collection(Category::withCount('courses')->get());;
+    //     return Resp(new HomeSection3Resource($data), 'success');
+    // }
+    // public function section4()
+    // {
+    //     $data['setting']    = getsetting('section4_setting', ['section4_status', 'section4_title', 'section4_body', 'section4_image']);
+    //     return Resp(new HomeSection4Resource($data), 'success');
+    // }
+    // public function section5()
+    // {
 
-        $data = [];
-        $data['setting']  = getsetting('section5_setting', ['section5_status', 'section5_title', 'section5_sub_title']);
-        $data['aboutus'] = AboutUs::with('user')->orderBy(DB::raw('RAND()'))->take(3)->get();
+    //     $data = [];
+    //     $data['setting']  = getsetting('section5_setting', ['section5_status', 'section5_title', 'section5_sub_title']);
+    //     $data['aboutus'] = AboutUs::with('user')->orderBy(DB::raw('RAND()'))->take(3)->get();
 
-        return Resp(new HomeSection5Resource($data), 'success');
-    }
-    public function section6()
-    {
+    //     return Resp(new HomeSection5Resource($data), 'success');
+    // }
+    // public function section6()
+    // {
 
-        $data = [];
-        $data['setting']  = getsetting('section6_setting', ['section6_status', 'section6_title', 'section6_sub_title']);
-        $data['blog'] = Blog::orderBy(DB::raw('RAND()'))->take(3)->get();
+    //     $data = [];
+    //     $data['setting']  = getsetting('section6_setting', ['section6_status', 'section6_title', 'section6_sub_title']);
+    //     $data['blog'] = Blog::orderBy(DB::raw('RAND()'))->take(3)->get();
 
-        return Resp(new HomeSection6Resource($data), 'success');
-    }
-    public function section7()
-    {
+    //     return Resp(new HomeSection6Resource($data), 'success');
+    // // }
+    // public function section7()
+    // {
 
-        $data = [];
-        $data['setting']  = getsetting('section7_setting', ['section7_status', 'section7_title', 'section7_sub_title']);
-        $data['fqa'] = Fqa::where('pin', 1)->get();
+    //     $data = [];
+    //     $data['setting']  = getsetting('section7_setting', ['section7_status', 'section7_title', 'section7_sub_title']);
+    //     $data['fqa'] = Fqa::where('pin', 1)->get();
 
-        return Resp(new HomeSection7Resource($data), 'success');
-    }
-    public function section8()
-    {
+    //     return Resp(new HomeSection7Resource($data), 'success');
+    // }
+    // public function section8()
+    // {
 
-        $data = [];
-        $data['setting']  = getsetting('section8_setting', ['section8_status', 'section8_title', 'section8_sub_title']);
+    //     $data = [];
+    //     $data['setting']  = getsetting('section8_setting', ['section8_status', 'section8_title', 'section8_sub_title']);
 
-        return Resp(new HomeSection8Resource($data), 'success');
-    }
+    //     return Resp(new HomeSection8Resource($data), 'success');
+    // }
 
 
 
