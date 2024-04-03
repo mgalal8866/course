93b5d2ddf3b48c0edb29a1361a4305bcdbc2574c
mgalal8866 <mgalal8866@gmail.com>
2024-04-03 04:45:43 +0200

    modified:   app/Http/Resources/ResultQuizResource.php

--- app/Http/Resources/ResultQuizResource.php
+++ app/Http/Resources/ResultQuizResource.php
@@ -15,7 +15,7 @@ class ResultQuizResource extends JsonResource
             'id'                => $this->id??'',
             'mark'              => '( ' .( isset($this->id)? $this->quiz_result_details->sum('marks'):'0') . ' / ' . (isset($this->id)?$this->quiz->total_marks:'0') . ' )',
             'Result'            => '( ' . (isset($this->id)? $this->quiz->question->count():'0') . ' / ' . (isset($this->id) ? $this->quiz_result_details->count() : '0') . ' )' ,
-            'Result'            =>  ((isset($this->id) ? $this->quiz_result_details->count() : '0')/ (isset($this->id)? $this->quiz->question->count():'0') * 100) > $this->quiz->redirect_mark ? $this->quiz->redirect_to_up: $this->quiz->redirect_to_down ,
+            'redirect'            =>  ((isset($this->id) ? $this->quiz_result_details->count() : '0')/ (isset($this->id)? $this->quiz->question->count():'0') * 100) > $this->quiz->redirect_mark ? $this->quiz->redirect_to_up: $this->quiz->redirect_to_down ,
             'history'           => $this->history??'',
         ];
     }
