1957fbb55f8d9d45d09b27574692e6101dfcaf06
mgalal8866 <mgalal8866@gmail.com>
2024-04-03 05:13:59 +0200

    modified:   app/Http/Resources/ResultQuizResource.php

--- app/Http/Resources/ResultQuizResource.php
+++ app/Http/Resources/ResultQuizResource.php
@@ -16,8 +16,8 @@ class ResultQuizResource extends JsonResource
             'mark'              => '( ' .( isset($this->id)? $this->quiz_result_details->sum('marks'):'0') . ' / ' . (isset($this->id)?$this->quiz->total_marks:'0') . ' )',
             'Result'            => '( ' . (isset($this->id)? $this->quiz->question->count():'0') . ' / ' . (isset($this->id) ? $this->quiz_result_details->count() : '0') . ' )' ,
             'percent'            => ( ((isset($this->id) ? $this->quiz_result_details->sum('marks') : '0')/ (isset($this->id)? $this->quiz->total_marks:'0')) * 100 ).'%',
-            'redirect'            =>  (((isset($this->id) ? $this->quiz_result_details->sum('marks') : '0')/ (isset($this->id)? $this->quiz->total_marks:'0') )* 100) > $this->quiz->redirect_mark ? $this->quiz->redirect_to_up: $this->quiz->redirect_to_down  ,
-            'redirect_name'            =>  (((isset($this->id) ? $this->quiz_result_details->sum('marks') : '0')/ (isset($this->id)? $this->quiz->total_marks:'0') )* 100) > $this->quiz->redirect_mark ? $this->quiz->redirect_up->name : $this->quiz->redirect_down->name  ,
+            'redirect'            =>  (((isset($this->id) ? $this->quiz_result_details->sum('marks') : '0')/ (isset($this->id)? $this->quiz->total_marks:'0') )* 100) > $this->quiz->redirect_mark ?( $this->quiz->redirect_to_up??''): ($this->quiz->redirect_to_down??'')  ,
+            'redirect_name'            =>  (((isset($this->id) ? $this->quiz_result_details->sum('marks') : '0')/ (isset($this->id)? $this->quiz->total_marks:'0') )* 100) > $this->quiz->redirect_mark ?( $this->quiz->redirect_up->name??'')  : ($this->quiz->redirect_down->name??'')  ,
             'history'           => $this->history??'',
         ];
     }
