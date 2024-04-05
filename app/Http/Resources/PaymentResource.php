07aa38df08a3da602fe3cbaa9e0d5e191b06c4e3
mgalal8866 <mgalal8866@gmail.com>
2024-04-06 01:55:12 +0200

    modified:   app/Http/Resources/PaymentResource.php

--- app/Http/Resources/PaymentResource.php
+++ app/Http/Resources/PaymentResource.php
@@ -19,9 +19,10 @@ class PaymentResource extends JsonResource
             'account_name'   => $this->account_name,
             'account_number' => $this->account_number,
             'iban_number'    => $this->iban_number,
+            
             'api_key'        => $this->api_key??'',
             'secret_key'     => $this->secret_key??'',
-            'active'         => $this->active??'',
+            // 'active'         => $this->active??'',
         ];
     }
 }
