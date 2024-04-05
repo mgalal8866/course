c80f97e08907c6175870439d86a104428066c660
mgalal8866 <mgalal8866@gmail.com>
2024-04-06 01:53:27 +0200

    modified:   app/Http/Controllers/Api/V1/PaymentsController.php 	new file:   app/Http/Resources/PaymentResource.php

--- app/Http/Controllers/Api/V1/PaymentsController.php
+++ app/Http/Controllers/Api/V1/PaymentsController.php
@@ -1,15 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\Api\V1;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PaymentResource;
 use App\Models\PaymentMethods;
 use App\Repositoryinterface\CourseRepositoryinterface;
 
 class PaymentsController extends Controller
 {
 
-public function get_payment(){
-    return PaymentMethods::get();
-}
+    public function get_payment()
+    {
+        return PaymentResource::collection(PaymentMethods::get());
+    }
 }
---
+++ app/Http/Resources/PaymentResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PaymentResource extends JsonResource
+{
+
+    public function toArray(Request $request): array
+    {
+
+        return [
+            'id'             => $this->id,
+            'type'           => $this->type,
+            'image'          => $this->image,
+            'name'           => $this->name,
+            'account_name'   => $this->account_name,
+            'account_number' => $this->account_number,
+            'iban_number'    => $this->iban_number,
+            'api_key'        => $this->api_key,
+            'secret_key'     => $this->secret_key,
+            'active'         => $this->active,
+        ];
+    }
+}
