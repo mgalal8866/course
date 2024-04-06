4b9d407cb1349bdc84e48a1538bb5f707e089c72
mgalal8866 <mgalal8866@gmail.com>
2024-04-06 05:14:16 +0200

    modified:   app/Http/Resources/CountryResource.php

--- app/Http/Resources/CountryResource.php
+++ app/Http/Resources/CountryResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Lang;
+use Stevebauman\Location\Facades\Location;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CountryResource extends JsonResource
@@ -13,13 +14,14 @@ class CountryResource extends JsonResource
     public function toArray(Request $request): array
     {
 
+        $s =  json_decode( json_encode(Location::get($request->ip())), true);
         return [
             'country_id'        => $this->id ,
             'country_name'      => $this->name ,
             'country_phonecode' => $this->phonecode ,
             'country_code'      => $this->iso2 ,
             'is_default'        => $this->iso2  == $request->session()->get('country') ?'1':'0',
-            // 'country_code3'     => $this->iso3 ,
+            'country_from_ip'   => $s['countryName'] . ' - ' . $s['regionName'] ,
             'country_flag'      => asset('asset/flag/country-') .Str::lower($this->iso2).'.svg' ,
             'country_currency'  => $this->currency[Lang::locale()] ,
         ];
