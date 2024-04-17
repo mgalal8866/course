8b0671420531eb94d61ccc32e45f82453c4ef9a7
mgalal8866 <mgalal8866@gmail.com>
2024-04-17 03:32:48 +0200

    modified:   app/Http/Resources/CountryResource.php

--- app/Http/Resources/CountryResource.php
+++ app/Http/Resources/CountryResource.php
@@ -15,14 +15,15 @@ class CountryResource extends JsonResource
     {
 
         $s =  json_decode( json_encode(Location::get($request->ip())), true);
-        dd($s);
+        // dd($s);
         $s??'';
         return [
             'country_id'        => $this->id ,
             'country_name'      => $this->name ,
             'country_phonecode' => $this->phonecode ,
             'country_code'      => $this->iso2,
-            'is_default'        => $this->id  == $request->header('country') ?'1':'0',
+            // 'is_default'        => $s['countryCode'] == $this->iso2 ?'1':'0' $this->id  == $request->header('country') ?'1':'0',
+            'is_default'        => isset($s['countryCode'])?( $s['countryCode'] == $this->iso2 ?'1':'0'):'',
             'country_from_ip'   => (isset($s['countryName'])?$s['countryName']:'') . ' - ' .  (isset($s['regionName'])?$s['regionName']:'') ,
             'country_flag'      => asset('asset/flag/country-') .Str::lower($this->iso2).'.svg' ,
             'country_currency'  => $this->currency[Lang::locale()] ,
