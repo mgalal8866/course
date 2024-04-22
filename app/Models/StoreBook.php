9764f4fc972bb29c8e64e0a1c64f426d8ebe8264
mgalal8866 <mgalal8866@gmail.com>
2024-04-23 01:21:48 +0200

    modified:   app/Http/Resources/BooksResource.php 	modified:   app/Models/StoreBook.php

--- app/Models/StoreBook.php
+++ app/Models/StoreBook.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Support\Facades\Auth;
 use App\Enum\TypeBook;
 use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,7 +11,7 @@ class StoreBook extends Model
 {
     use UUID,HasFactory;
     protected $guarded = [];
-    
+
     public function getImageurlAttribute()
     {
         return $this->image ? path('','book') . $this->image :path('','').'no-imag.png';
@@ -19,6 +19,10 @@ class StoreBook extends Model
     public function category()
     {
         return $this->belongsTo(CategoryBook::class, 'category_id');
+    }
+    public function orderdetails()
+    {
+        return $this->hasOne(OrdersDetails::class, 'product_id')->where('user_id',Auth::guard('student')->user()->id);
     }
 
 
