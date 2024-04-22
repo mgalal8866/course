0c0a1aff4a9f50aa6b346a2e442af186bf21fc58
mgalal8866 <mgalal8866@gmail.com>
2024-04-23 01:41:01 +0200

    modified:   app/Models/StoreBook.php

--- app/Models/StoreBook.php
+++ app/Models/StoreBook.php
@@ -22,7 +22,9 @@ class StoreBook extends Model
     }
     public function orderdetails()
     {
-        return $this->hasOne(OrdersDetails::class, 'product_id')->where('user_id',Auth::guard('student')->user()->id);
+        return $this->hasOne(OrdersDetails::class, 'product_id')->WhereHas('order', function($q){
+            $q->where('user_id', Auth::guard('student')->user()->id);
+        });
     }
 
 
