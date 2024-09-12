95e2b25c205cb89453d8fdbae3e68ba03dd617fd
Mohamed Galal <mgalal8866@gmail.com>
2024-09-12 03:55:41 +0300

    modified:   app/Http/Controllers/Api/V1/PaymentsController.php

--- app/Http/Controllers/Api/V1/PaymentsController.php
+++ app/Http/Controllers/Api/V1/PaymentsController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
+use App\Models\PaymentMethods;
+use App\Models\PaymentTransaction;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PaymentResource;
-use App\Models\PaymentMethods;
 use App\Repositoryinterface\CourseRepositoryinterface;
-use Illuminate\Support\Facades\Log;
 
 class PaymentsController extends Controller
 {
@@ -18,7 +19,9 @@ class PaymentsController extends Controller
     }
     public function payment_callback(Request $request)
     {
-        Log::error($request->all());
+        PaymentTransaction::where('response_data->status', operator: $request->status)
+    ->update(['response_data->data->invoice_id' => $request->invoice_id]);
+
         return    Resp( $request->all(), 'success', 200, true);
     }
 }
